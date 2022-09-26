45fbeafe5e8e8b495b1287afc425b0c85540e7e0
Matthew Kweon <kweon.matt@gmail.com>
2022-09-26 22:27:41 +0000

    create a new XMLHttpRequest using the value of the input from the search bar. issue #1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,22 @@
 var $searchBar = document.querySelector('#search-bar');
+var searchInput = '';
 
-$searchBar.addEventListener('input', function (event) {
-  // var searchInput = event.target.value;
+function getCocktailData(name) {
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'https://api.api-ninjas.com/v1/cocktail?name=' + name);
+  xhr.setRequestHeader('X-Api-Key', 'tsY43L3Lh5SZ/WR5FQznyA==QerTD0YdjkWIbYLP');
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', function () {
+  });
+  xhr.send();
+}
+
+$searchBar.addEventListener('input', function (e) {
+  searchInput = e.target.value;
+});
+
+$searchBar.addEventListener('keydown', function (e) {
+  if (e.key === 'Enter') {
+    getCocktailData(searchInput);
+  }
 });
