c6753fde873070283b13212126a47ea6456fcd86
Matthew Kweon <kweon.matt@gmail.com>
2022-09-26 22:55:36 +0000

    Verify that new search entries are saved to local storage along with the rest of the data model on page reload. issue #1

--- js/data.js
+++ js/data.js
@@ -5,3 +5,14 @@ var data = {
   library: [],
   nextEntryId: 1
 };
+
+var previousSearchInput = localStorage.getItem('recipe-local-storage');
+
+if (previousSearchInput !== null) {
+  data = JSON.parse(previousSearchInput);
+}
+
+window.addEventListener('beforeunload', function (e) {
+  var searchInput = JSON.stringify(data);
+  localStorage.setItem('recipe-local-storage', searchInput);
+});
