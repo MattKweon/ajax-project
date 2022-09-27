d142c054d6a9141127695e21a698f3326ac3347a
Matthew Kweon <kweon.matt@gmail.com>
2022-09-27 23:58:44 +0000

    Cocktail name with the name in data.recipe object.  Cocktail ingredients list with the values in data.recipe object.  Instructions to make cocktail in data.recipe object. issue #2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ var $searchDisplay = document.querySelector('.search-display');
 var $searchBar = document.querySelector('#search-bar');
 var $searchForm = document.querySelector('.search-form');
 var searchInput = '';
+// var ingredientList = data.recipe.ingredients;
 
 function showDisplay(e) {
   if (e.target.matches('#search-btn')) {
@@ -46,5 +47,42 @@ $searchForm.addEventListener('submit', function (e) {
 });
 
 // function createNewRecipe(entry) {
-
+//   var cardContainer = document.createElement('div');
+//   cardContainer.setAttribute('class', 'container recipe-card');
+//   var rowEl = document.createElement('div');
+//   rowEl.setAttribute('class', 'row');
+//   cardContainer.appendChild(rowEl);
+//   var colHalf = document.createElement('div');
+//   colHalf.setAttribute('class', 'col-half');
+//   rowEl.appendChild(colHalf);
+//   var imgContainer = document.createElement('div');
+//   imgContainer.setAttribute('class', 'img-container');
+//   colHalf.appendChild(imgContainer);
+//   var cocktailImg = document.createElement('img');
+//   cocktailImg.setAttribute('class', 'cocktail-img');
+//   cocktailImg.setAttribute('src', data.recipe.imgUrl);
+//   cocktailImg.setAttribute('alt', data.recipe.name);
+//   imgContainer.appendChild(cocktailImg);
+//   var newColHalf = document.createElement('div');
+//   newColHalf.setAttribute('class', 'col-half recipe-content');
+//   rowEl.appendChild(newColHalf);
+//   var cocktailName = document.createElement('h1');
+//   cocktailName.textContent = data.recipe.name;
+//   newColHalf.appendChild(cocktailName);
+//   var ingredients = document.createElement('h3');
+//   ingredients.textContent = 'Ingredients';
+//   newColHalf.appendChild(ingredients);
+//   var ul = document.createElement('ul');
+//   newColHalf.appendChild(ul);
+//   for (var i = 0; i < ingredientList.length; i++) {
+//     var li = document.createElement('li');
+//     li.textContent = ingredientList[i];
+//     ul.appendChild(li);
+//   }
+//   var instructions = document.createElement('h3');
+//   instructions.textContent = 'Instructions';
+//   newColHalf.appendChild(instructions);
+//   var p = document.createElement('p');
+//   p.textContent = data.recipe.instructions;
+//   return cardContainer;
 // }
