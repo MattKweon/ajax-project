89e5acdb2977dde5b914a2515e1a37804ace9150
Matthew Kweon <kweon.matt@gmail.com>
2022-09-28 00:24:33 +0000

    Add the DOM tree to the display view. issue #2

--- js/main.js
+++ js/main.js
@@ -2,7 +2,8 @@ var $searchDisplay = document.querySelector('.search-display');
 var $searchBar = document.querySelector('#search-bar');
 var $searchForm = document.querySelector('.search-form');
 var searchInput = '';
-// var ingredientList = data.recipe.ingredients;
+var $recipeDisplay = document.querySelector('.recipe-display');
+var ingredientList = data.recipe.ingredients;
 
 function showDisplay(e) {
   if (e.target.matches('#search-btn')) {
@@ -20,6 +21,7 @@ function getCocktailData(name) {
   xhr.addEventListener('load', function () {
     data.recipe = xhr.response[0];
     $searchForm.reset();
+    getCocktailImg(searchInput);
   });
   xhr.send();
 }
@@ -32,6 +34,7 @@ function getCocktailImg(name) {
   xhr.addEventListener('load', function () {
     var imgUrl = xhr.response.photos[0].src.original;
     data.recipe.imgUrl = imgUrl;
+    $recipeDisplay.append(createNewRecipe(data.recipe));
   });
   xhr.send();
 }
@@ -43,46 +46,45 @@ $searchBar.addEventListener('input', function (e) {
 $searchForm.addEventListener('submit', function (e) {
   e.preventDefault();
   getCocktailData(searchInput);
-  getCocktailImg(searchInput);
 });
 
-// function createNewRecipe(entry) {
-//   var cardContainer = document.createElement('div');
-//   cardContainer.setAttribute('class', 'container recipe-card');
-//   var rowEl = document.createElement('div');
-//   rowEl.setAttribute('class', 'row');
-//   cardContainer.appendChild(rowEl);
-//   var colHalf = document.createElement('div');
-//   colHalf.setAttribute('class', 'col-half');
-//   rowEl.appendChild(colHalf);
-//   var imgContainer = document.createElement('div');
-//   imgContainer.setAttribute('class', 'img-container');
-//   colHalf.appendChild(imgContainer);
-//   var cocktailImg = document.createElement('img');
-//   cocktailImg.setAttribute('class', 'cocktail-img');
-//   cocktailImg.setAttribute('src', data.recipe.imgUrl);
-//   cocktailImg.setAttribute('alt', data.recipe.name);
-//   imgContainer.appendChild(cocktailImg);
-//   var newColHalf = document.createElement('div');
-//   newColHalf.setAttribute('class', 'col-half recipe-content');
-//   rowEl.appendChild(newColHalf);
-//   var cocktailName = document.createElement('h1');
-//   cocktailName.textContent = data.recipe.name;
-//   newColHalf.appendChild(cocktailName);
-//   var ingredients = document.createElement('h3');
-//   ingredients.textContent = 'Ingredients';
-//   newColHalf.appendChild(ingredients);
-//   var ul = document.createElement('ul');
-//   newColHalf.appendChild(ul);
-//   for (var i = 0; i < ingredientList.length; i++) {
-//     var li = document.createElement('li');
-//     li.textContent = ingredientList[i];
-//     ul.appendChild(li);
-//   }
-//   var instructions = document.createElement('h3');
-//   instructions.textContent = 'Instructions';
-//   newColHalf.appendChild(instructions);
-//   var p = document.createElement('p');
-//   p.textContent = data.recipe.instructions;
-//   return cardContainer;
-// }
+function createNewRecipe(entry) {
+  var cardContainer = document.createElement('div');
+  cardContainer.setAttribute('class', 'container recipe-card');
+  var rowEl = document.createElement('div');
+  rowEl.setAttribute('class', 'row');
+  cardContainer.appendChild(rowEl);
+  var colHalf = document.createElement('div');
+  colHalf.setAttribute('class', 'col-half');
+  rowEl.appendChild(colHalf);
+  var imgContainer = document.createElement('div');
+  imgContainer.setAttribute('class', 'img-container');
+  colHalf.appendChild(imgContainer);
+  var cocktailImg = document.createElement('img');
+  cocktailImg.setAttribute('class', 'cocktail-img');
+  cocktailImg.setAttribute('src', data.recipe.imgUrl);
+  cocktailImg.setAttribute('alt', data.recipe.name);
+  imgContainer.appendChild(cocktailImg);
+  var newColHalf = document.createElement('div');
+  newColHalf.setAttribute('class', 'col-half recipe-content');
+  rowEl.appendChild(newColHalf);
+  var cocktailName = document.createElement('h1');
+  cocktailName.textContent = data.recipe.name;
+  newColHalf.appendChild(cocktailName);
+  var ingredients = document.createElement('h3');
+  ingredients.textContent = 'Ingredients';
+  newColHalf.appendChild(ingredients);
+  var ul = document.createElement('ul');
+  newColHalf.appendChild(ul);
+  for (var i = 0; i < ingredientList.length; i++) {
+    var li = document.createElement('li');
+    li.textContent = ingredientList[i];
+    ul.appendChild(li);
+  }
+  var instructions = document.createElement('h3');
+  instructions.textContent = 'Instructions';
+  newColHalf.appendChild(instructions);
+  var p = document.createElement('p');
+  p.textContent = data.recipe.instructions;
+  return cardContainer;
+}
